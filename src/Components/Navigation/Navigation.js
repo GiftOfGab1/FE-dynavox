a5737cb8f5cc8746456df513f0b98a8c5e5e45b1
Jeremiah Black <56662070+jeremiahblackol@users.noreply.github.com>
2020-09-07 13:42:51 -0600

    Feature/add styling (#26)
    * fixing merge conslicts
    
    * fix merge conflicts
    
    * add some basic styling
    
    * adjusted styling
    
    * styles continued
    
    * added border radius to play and save buttons

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import Button from '../Button/Button'
-import './Navigation.css';
+import Button from '../Button/button'
+import './navigation.css';
 
 
 function Navigation() {
 
     return (
         <nav className='navigation'>
-            <h1 className='header'>Gift of Gab</h1>
             <section className='button-container'>
                 <Button label={'Home'}/>
                 <Button label={'Options'}/>

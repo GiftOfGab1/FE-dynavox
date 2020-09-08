fcc377312cc18c8cd3bd77ad9958a642858e2e13
jdvolk <justin.d.volk@gmail.com>
2020-09-08 11:49:48 -0600

    set up store folder with reducers and actions, add actions for updating the user... etc

--- src/Reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-// We will need to import reducers as we add them
-
-const rootReducer = combineReducers({
-// reducers will go here
-})
-
-export default rootReducer

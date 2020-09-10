220736d1c74955458379eef7662da464da22f64e
Justin Volk <54607724+jdvolk@users.noreply.github.com>
2020-09-10 17:20:30 -0600

    Feature/icons (#44)
    * add post request for updating user settings
    
    * add reducer for updating user
    
    * add actions/ reducers for a post request to change user voice settings
    
    * fixed travis errors
    
    * add bank and party icons
    
    * add bank icon and party icon

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -2,15 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import SubCategory from '../SubCategory/subCategory'
 import './MainPage.css';
-
+import bankImage from '../../Assets/SocialSettings/bank.png'
+import partyImage from '../../Assets/SocialSettings/party.png'
 
 function MainPage() {
 return (
     <section className='main-page-container'>
+      <div className='glass'>
         <Link to="/phrases-page" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <SubCategory categoryName={'Bank'}/>
+            <SubCategory 
+              categoryName={'Bank'}
+              img={bankImage}
+            />
         </Link>
-        <SubCategory categoryName={'Grocery Store'}/>
+        <SubCategory 
+          categoryName={'Party'}
+          img={partyImage}
+        />
+      </div>
     </section>
 )
 }

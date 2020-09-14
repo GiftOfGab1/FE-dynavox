b96ae6938c3b462843713621cfb8ec9ce34a4efb
Joe Haefling <josephhaefling@gmail.com>
2020-09-14 12:54:42 -0600

    Add back button to subCategories page

--- src/Components/PhrasesPage/usePhrasePage.js
+++ src/Components/PhrasesPage/usePhrasePage.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Phrase from '../Phrase/Phrase'
+import { Link } from 'react-router-dom'
+
 
 function usePhrasePage(id, name, socialSettings) {
+    console.log(id);
+    console.log(name);
     const currentSetting = socialSettings.find(socialSetting => socialSetting.title === name)
     const subSettingPhrases = currentSetting.phrases.filter(phrase => phrase.tags[0] === id)
-    return subSettingPhrases.map(phrase => <Phrase categoryName={phrase}  phraseName={phrase.expression}  />
+    return subSettingPhrases.map(phrase => {
+        return (<Link
+            key={'name'}
+            to={`/subCategories-page/${name}`}
+            style={{ textDecoration: 'none', color: 'inherit' }}
+        >
+            <Phrase categoryName={phrase}  phraseName={phrase.expression}  />
+        </Link>)
+    }
 )
 } 
 

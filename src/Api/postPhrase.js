3a9c6739baa4b755c783e57620b5fc976d0d7569
jdvolk <justin.d.volk@gmail.com>
2020-09-15 16:28:56 -0600

    fixed more lint errors

--- src/Api/postPhrase.js
+++ src/Api/postPhrase.js
@@ -1,7 +1,7 @@
 export const postPhrase = async (expression, image, tags, section ) => {
   console.log(section)
   let  sectionId;
-  section == "Bank" ? sectionId = 1 : sectionId = 2
+  section === "Bank" ? sectionId = 1 : sectionId = 2
   try {
       const response = await fetch('https://gift-of-gab.herokuapp.com/v1/graphql', {
           method: 'POST',

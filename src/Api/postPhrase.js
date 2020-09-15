ad05835e99ee469ac45777b9f1b9c7821741da39
jdvolk <justin.d.volk@gmail.com>
2020-09-15 15:57:58 -0600

    add post request to add a phrase

---
+++ src/Api/postPhrase.js
@@ -0,0 +1,26 @@
+export const postPhrase = async (expression, image, tags, section ) => {
+  let  sectionId
+  section === "Bank" ? sectionId = 0 : sectionId = 1
+  try {
+      const response = await fetch('https://gift-of-gab.herokuapp.com/v1/graphql', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+          },
+          body: JSON.stringify({
+              query: `
+                mutation { 
+                  newPhrase(input: 
+                    {expression: "${expression}", image: "${image}", tags: "${tags}", sectionId: "${sectionId}" })
+                    { phrase { id, expression, image, tags } }
+
+                  }`
+          })
+      })
+      return response.json()
+  } catch (err) {
+      console.log(err)
+  }
+  
+}
